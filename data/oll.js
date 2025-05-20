cb3a7781f2eb047b0d326f7e4c8b9d7c099e4ce2
jimat99 <nggocal99@gmail.com>
2025-05-20 17:37:16 +0700

    First commit using Git

---
+++ data/oll.js
@@ -0,0 +1,130 @@
+const ollCases = [
+  {
+    id: "oll21",
+    name: "OLL 21",
+    setup: "R U R' U R U' R' U R U2' R' y'",
+    algorithm: "y F R U R' U' R U R' U' R U R' U' F'",
+    image: "image/oll/oll-21.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_21"
+  },
+  {
+    id: "oll22",
+    name: "OLL 22",
+    setup: "R' U2' R2' U R2' U R2' U2' R'",
+    algorithm: "f R U R' U' f' F R U R' U' F'",
+    image: "image/oll/oll-22.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_22"
+  },
+  {
+    id: "oll23",
+    name: "OLL 23",
+    setup: "R U2' R D R' U2' R D' R2'",
+    algorithm: "R2 D R' U2 R D' R' U2 R'",
+    image: "image/oll/oll-23.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_23"
+  },
+  {
+    id: "oll24",
+    name: "OLL 24",
+    setup: "F R' F' r U R U' r'",
+    algorithm: "r U R' U' r' F R F'",
+    image: "image/oll/oll-24.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_24"
+  },
+  {
+    id: "oll25",
+    name: "OLL 25",
+    setup: "R' F' r U R U' r' F y'",
+    algorithm: "y F' r U R' U' r' F R",
+    image: "image/oll/oll-25.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_25"
+  },
+  {
+    id: "oll26",
+    name: "OLL 26",
+    setup: "R U R' U R U2' R' y'",
+    algorithm: "R' U' R U' R' U2 R",
+    image: "image/oll/oll-26.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_26"
+  },
+  {
+    id: "oll27",
+    name: "OLL 27",
+    setup: "R U2' R' U' R U' R'",
+    algorithm: "R U R' U R U2 R'",
+    image: "image/oll/oll-27.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_27"
+  },
+  {
+    id: "oll28",
+    name: "OLL 28",
+    setup: "R U R' U' M' U R U' r'",
+    algorithm: "r U R' U' M U R U' R'",
+    image: "image/oll/oll-28.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_28"
+  },
+  {
+    id: "oll33",
+    name: "OLL 33",
+    setup: "F R' F' R U R U' R'",
+    algorithm: "R U R' U' R' F R F'",
+    image: "image/oll/oll-33.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_33"
+  },
+  {
+    id: "oll43",
+    name: "OLL 43",
+    setup: "f' U' L' U L f",
+    algorithm: "f' L' U' L U f",
+    image: "image/oll/oll-43.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_43"
+  },
+  {
+    id: "oll44",
+    name: "OLL 44",
+    setup: "f U R U' R' f'",
+    algorithm: "f R U R' U' f'",
+    image: "image/oll/oll-44.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_44"
+  },
+  {
+    id: "oll45",
+    name: "OLL 45",
+    setup: "F U R U' R' F'",
+    algorithm: "F R U R' U' F'",
+    image: "image/oll/oll-45.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_45"
+  },
+  {
+    id: "oll51",
+    name: "OLL 51",
+    setup: "f U R U' R' U R U' R' f'",
+    algorithm: "f R U R' U' R U R' U' f'",
+    image: "image/oll/oll-51.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_51"
+  },
+  {
+    id: "oll55",
+    name: "OLL 55",
+    setup: "F R' F' U2' R U R' U R2' U2' R'",
+    algorithm: "y F R U R' U' S R U R' U' S' R U R' U' F'",
+    image: "image/oll/oll-55.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_55"
+  },
+  {
+    id: "oll56",
+    name: "OLL 56",
+    setup: "r U r' R U R' U' R U R' U' r U' r'",
+    algorithm: "y F R U R' U' R U R' U' S R U R' U' f'",
+    image: "image/oll/oll-56.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_56"
+  },
+  {
+    id: "oll57",
+    name: "OLL 57",
+    setup: "r U R' U' M U R U' R'",
+    algorithm: "R U R' U' M' U R U' r'",
+    image: "image/oll/oll-57.PNG",
+    source: "https://speedcubedb.com/a/3x3/OLL/OLL_57"
+  },
+];
