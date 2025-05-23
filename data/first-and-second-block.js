d4d6aef7ad6cf19a99705bd9e3f780f49bd13437
jimat99 <nggocal99@gmail.com>
2025-05-23 18:07:40 +0700

    5) Add second block

---
+++ data/first-and-second-block.js
@@ -0,0 +1,37 @@
+const firstBlockCases = [
+  {
+    algorithm: "l' U' l",
+    image: "image/cmll/block/first-block-1.png",
+  },
+  {
+    algorithm: "U' L' U L",
+    image: "image/cmll/block/first-block-2.png",
+  },
+  {
+    algorithm: "L' U' L",
+    image: "image/cmll/block/first-block-3.png",
+  },
+  {
+    algorithm: "M' l' U' l",
+    image: "image/cmll/block/first-block-4.png",
+  },
+];
+
+const secondBlockCases = [
+  {
+    algorithm: "r U r'",
+    image: "image/cmll/block/second-block-1.png",
+  },
+  {
+    algorithm: "U R U' R'",
+    image: "image/cmll/block/second-block-2.png",
+  },
+  {
+    algorithm: "R U R'",
+    image: "image/cmll/block/second-block-3.png",
+  },
+  {
+    algorithm: "M' r U r'",
+    image: "image/cmll/block/second-block-4.png",
+  },
+];
